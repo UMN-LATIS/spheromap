365966f93cc6d03966509c705cf7cde15697569b
Alison Link <linkalis@gmail.com>
2016-04-04 22:02:29 -0500

    Added rotations to photos

--- app.js
+++ app.js
@@ -8,24 +8,27 @@ var app = express();
 
 // APP SETTINGS
 
-app.set('title', 'China 2016');
-
+// handle views & templates
 app.engine('html', consolidate.handlebars);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
+// set asset paths
 app.use('/photos', express.static(__dirname + '/photos'));
 app.use('/assets', express.static(__dirname + '/assets'));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 
-// CONFIG - to load user-defined settings from config.js
+// CONFIG - load user-defined settings from config.js
 
 var config = require('./config.json');
 
+// then set the app title
+app.set('title', config.title);
 
-// JSON DATA for PHOTO TOURS - load into app.locals to pass as JSON into the views
+
+// JSON DATA for PHOTO TOURS - load this data into app.locals to pass as JSON into the views
 
 app.locals.photo_tours = require("./js/photo_tour_data.json");
 
@@ -61,22 +64,26 @@ app.get('/:tour', function (req, res) {
 
 app.get('/:tour/photosphere/:image', function (req, res) {
   // Filter with lodash to find image-specific data in the JSON,
-  // then get rotation from that to pass to the view
+  // then get rotation from that to pass to the view.
+  // This feels like overkill, but eh.
   var tour_data = _.find(req.app.locals.photo_tours, { tour_id: req.params.tour });
-  var photo_data = _.find(tour_data, { photos: [{ filename: req.params.image }] });
-  console.log(tour_data.photos);
-  console.log(photo_data);
-  //console.log(image_data.rotation);
+  var photo_data = _.find(tour_data.photos, { filename: req.params.image });
+  var photo_rotation = photo_data.rotation;
 
   res.render('photosphere', {
     src: '/photos/' + req.params.image,
-    tour_id: req.params.tour
-    //rotation: rotation
+    tour_id: req.params.tour,
+    rotation: photo_rotation,
+    title: config.title
   });
 });
 
+// plan photosphere route for showing un-rotated images outside of tours
 app.get('/photosphere/:image', function (req, res) {
-  res.render('photosphere', { src: '/photos/' + req.params.image });
+  res.render('photosphere', {
+    src: '/photos/' + req.params.image,
+    title: config.title
+  });
 });
 
 app.listen(3000, function () {
